825a65c3fd82dabc7d6b37958bdf970257119bc1
Jeff <jeff.uca@gmail.com>
2019-08-23 20:54:33 +0200

    fix

--- knapsack_problem.py
+++ knapsack_problem.py
@@ -1,43 +1,24 @@
 
-
 def knapsack(valor, peso, numero_objetos, capacidad):
     f = [[0] * capacidad] * numero_objetos
     resultado = None
     for j in range(0, capacidad):
         if j < peso[j]:
-            f[0,j] = 0
+            f[0][j] = 0
         else:
-            f[0 ,j] = valor[j]
+            f[0 ][j] = valor[j]
 
-
-    for i in range(0,numero_objetos):
+    for i in range(0, numero_objetos):
         for j in range(0, capacidad):
             if j < peso[i]:
-                f[i,j] = f[i-1, j]
+                f[i][ j] = f[i - 1][ j]
             else:
-                f[i,j] = max(f[i-1, j], f[i,j-peso[i]]+valor[i])
+                f[i][ j] = max(f[i - 1][ j], f[i][ j - peso[i]] + valor[i])
 
-    resultado = f[numero_objetos, capacidad]
+    resultado = f[numero_objetos][ capacidad]
 
     return resultado
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
-    knapsack(1,10,1,1,4)
-
-
-
-
-
-
-
-
+    knapsack(1, 10, 1, 1, 4)
